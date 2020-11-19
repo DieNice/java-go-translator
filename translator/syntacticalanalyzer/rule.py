bad7f7a7491811788059cff0b030e55f7e546bdf
DieNice <DailyPlanning@bk.ru>
2020-11-19 12:36:22 +1000

    added class rule

---
+++ translator/syntacticalanalyzer/rule.py
@@ -0,0 +1,13 @@
+class Rule(object):
+    def __init__(self, name, *productions):
+        self.name = name
+        self.productions = list(productions)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "%s -> %s" % (self.name, " | ".join(repr(p) for p in self.productions))
+
+    def add(self, *productions):
+        self.productions.extend(productions)
