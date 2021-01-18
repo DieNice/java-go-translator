fa7f453a594baf251334ce2cdcd071aa2a99bd80
eswat109 <eswat98@mail.ru>
2021-01-18 21:22:01 +1000

    added catch in main

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ if __name__ == '__main__':
     indata = reader.read()
     if type(indata) is list:
         for i in indata:
-            res = lexer.skan(i)
-            print('file: {},data {}'.format(i[0], res[0]))
-            textnow = synanalyzer.lextableToString(res[0])
-            earlyres = synanalyzer.earley(rule=synanalyzer.PROGRAMM, text=textnow)
-            tree = synanalyzer.right_parsing(earlyres)
-            print(tree)
+            try:
+                res = lexer.skan(i)
+                print('file: {},data {}'.format(i[0], res[0]))
+                textnow = synanalyzer.lextableToString(res[0])
+                earlyres = synanalyzer.earley(rule=synanalyzer.PROGRAMM, text=textnow)
+                tree = synanalyzer.right_parsing(earlyres)
+                print(tree)
+            except:
+                print('error in file: {}'.format(i[0]))
     else:
         lexer.skan(indata)
