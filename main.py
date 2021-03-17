1ba6d542472cc03b2a249eb38695aee8283a9b33
DieNice <DailyPlanning@bk.ru>
2021-03-17 16:40:33 +1000

    fix problems, refactored code

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ if __name__ == '__main__':
     lexer = LexicalAnalyzer()
     synanalyzer = SyntacticalAnalyzer()
     reader = FileReader()
-    indata = reader.read()
+    indata = reader.read(namepattern='Formula3.java') #[^E].*
     if type(indata) is list:
         for i in indata:
-            try:
-                res = lexer.skan(i)
-                print('file: {},data {}'.format(i[0], res[0]))
-                textnow = synanalyzer.lextableToString(res[0])
-                earlyres = synanalyzer.earley(rule=synanalyzer.PROGRAMM, text=textnow)
-                parselist = synanalyzer.right_parsing(earlyres)
-                print(parselist)
-                dirtytree = synanalyzer.toTree(parselist)
-                dirtytree.printTree()
-                ast = SyntacticsStructure(dirtytree)
-                ast.printast()
-            except:
-                print('error in file: {}'.format(i[0]))
+            # try:
+            res = lexer.skan(i)
+            print('file: {},data {}'.format(i[0], res[0]))
+            textnow = synanalyzer.lextableToString(res[0])
+            earlyres = synanalyzer.earley(rule=synanalyzer.PROGRAMM, text=textnow)
+            parselist = synanalyzer.right_parsing(earlyres)
+            print(parselist)
+            dirtytree = synanalyzer.toTree(parselist)
+            dirtytree.printTree()
+            ast = SyntacticsStructure(dirtytree)
+            ast.printast()
+        # except:
+        #     print('error in file: {}'.format(i[0]))
     else:
         lexer.skan(indata)
--- translator/syntacticalanalyzer/semanticanalyzer/syntacticstructure.py
+++ translator/syntacticalanalyzer/semanticanalyzer/syntacticstructure.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ...lexicalanalyzer.env import Env
 from ...lexicalanalyzer.idtable import IdTable
 from ..recognizer.syntacticaltree import SyntacticalTree
@@ -6,12 +7,12 @@ from ..recognizer.rule import Rule
 
 
 class NodeStruct:
-    def __init__(self, isNonterminal: bool, name: str, value: str = '', prev=None) -> None:
+    def __init__(self, isNonterminal: bool, name: str, value: str = '', prev: NodeStruct = None) -> None:
         self.name = name
         self.value = value
         self.isNonterminal = isNonterminal
-        self.prev = prev
-        self.childs = []
+        self.prev: NodeStruct = prev
+        self.childs: list(NodeStruct) = []
 
     def __str__(self):
         nonterm = "Nonterminal"
@@ -20,10 +21,44 @@ class NodeStruct:
         res = '[' + nonterm + ';' + self.name + ';' + self.value + ']'
         return res
 
+    def __repr__(self):
+        nonterm = "Nonterminal"
+        if self.isNonterminal == False:
+            nonterm = "Terminal"
+        res = '[' + nonterm + ';' + self.name + ';' + self.value + ']'
+        return res
+
+    @property
+    def me(self) -> NodeStruct:
+        for i in range(len(self.prev.childs)):
+            if self == self.prev.childs[i]:
+                return self.prev.childs[i]
+
+    @me.setter
+    def me(self, newNode) -> None:
+        if self.prev == None:
+            self.childs = newNode.childs
+            self.value = newNode.value
+            self.isNonterminal = newNode.isNonterminal
+            self.name = newNode.name
+        else:
+            for i in range(len(self.prev.childs)):
+                if self == self.prev.childs[i]:
+                    self.prev.childs[i].childs = newNode.childs
+                    self.prev.childs[i].value = newNode.value
+                    self.prev.childs[i].isNonterminal = newNode.isNonterminal
+                    self.prev.childs[i].name = newNode.name
+                    break
+
+    @me.deleter
+    def me(self):
+        del self
+
 
 class SyntacticsStructure:
     def __init__(self, stree: SyntacticalTree):
-        self.uselessterms = ["public", "void", "main", "(", "String[]", "args", ")", "{", "}", "class", ",", "if",
+        self.uselessterms = ["public", "static", "void", "main", "(", "String[]", "args", ")", "{", "}", "class", ",",
+                             "if",
                              "System.out.print", "System.out.println", "\'", "for", "while", "\"", ";", "do"]
         self.operations = ['+', '-', '*', '/', '%', '++', '--', '>', '<', '>=', '<=', '==', '!=', '&&', '||', '!', '=']
         self.root = self._copytree(stree)
@@ -31,17 +66,24 @@ class SyntacticsStructure:
 
     def _isoperation(self, ptr: NodeStruct) -> bool:
         '''Check if the node has signed descendants'''
-        for i in ptr.childs:
-            if i.name in self.operations:
+        if self._havealonechild(ptr):
+            if ptr.childs[0].name in self.operations:
                 return True
-        return False
+        else:
+            opcount = 0
+            for i in ptr.childs:
+                if i.name in self.operations:
+                    opcount += 1
+            return opcount == 1
 
     def _deleteoperationterm(self, ptr: NodeStruct) -> None:
         '''Delete operation term in Node'''
         for i in ptr.childs:
             if i.name in self.operations:
-                ptr.name = i.name
+                ptr.me.name = i.name
+                ptr.me.isNonterminal = i.isNonterminal
                 ptr.childs.remove(i)
+                break
 
     def _haveuselessterm(self, ptr: NodeStruct):
         '''checks the tree for useless symbols'''
@@ -54,9 +96,12 @@ class SyntacticsStructure:
 
     def _deleteuslessterm(self, ptr: NodeStruct) -> None:
         '''Delete useless terminal symbol'''
-        for i in ptr.childs:
-            if i.name in self.uselessterms:
-                ptr.remove(i)
+        for j in range(len(ptr.prev.childs)):
+            if ptr.prev.childs[j] == ptr:
+                for i in ptr.childs:
+                    if i.name in self.uselessterms:
+                        ptr.prev.childs[j].childs.remove(i)
+                        break
                 break
 
     def _checkcomplete(self) -> bool:
@@ -67,8 +112,7 @@ class SyntacticsStructure:
                 return False
             if ptr.childs:
                 for i in ptr.childs:
-                    if _search(i, newnode) == True:
-                        return False
+                    return _search(i)
             return True
 
         return _search(self.root)
@@ -85,42 +129,40 @@ class SyntacticsStructure:
                 return False
             if ptr.childs:
                 for i in ptr.childs:
-                    if _search(i, newnode) == True:
+                    if _search(i) == True:
                         return False
             return True
 
         return _search(ptr)
 
     def _replace_node(self, parent: NodeStruct, newNode: NodeStruct) -> None:
-        for i in range(len(parent.prev.childs)):
-            if parent == parent.prev.childs[i]:
-                prevbuf = parent.prev
-                parent.prev.childs[i] = newNode
-                parent.prev.childs[i].prev = prevbuf
-                break
+        '''Replacing a node with a new node'''
+        parent.me = newNode
 
     def _reformattree(self, ptr: NodeStruct) -> None:
-        '''Algorithm for converting an output tree into an operation tree https://studopedia.su/14_133217_derevo-razbora-preobrazovanie-dereva-razbora-v-derevo-operatsiy.html'''
+        '''Modification of algorithm for converting an output tree into an operation tree https://studopedia.su/14_133217_derevo-razbora-preobrazovanie-dereva-razbora-v-derevo-operatsiy.html'''
         while not self._checkcomplete():  # step 1
             while True:
                 lastnode = self.getlastnonterm(ptr)  # step 2
                 if self._havealonechild(lastnode):  # step3
-                    self._replace_node(lastnode, lastnode.childs[0])
-                    # return to step1
+                    lastnode.me = lastnode.childs[0]
+                    self._reformattree(ptr)  # return to step1
+                elif self._haveuselessterm(lastnode):  # step 4
+                    self._deleteuslessterm(lastnode)
+                elif self._isoperation(lastnode):  # step 5
+                    self._deleteoperationterm(lastnode)
                     self._reformattree(ptr)
+                elif lastnode.isNonterminal == True and lastnode.name == "IDENTIFICATOR" and lastnode.prev.name == "IDENTIFICATOR":
+                    prevbuf = lastnode.prev
+                    lastnode.prev.childs.remove(lastnode)
+                    for child in lastnode.childs:
+                        child.prev = prevbuf
+                        prevbuf.childs.append(child)
+                elif lastnode.isNonterminal == True:
+                    lastnode.me.isNonterminal = False
+                elif not self._havenonterminalchilds(lastnode):  # step 6
                     break
-                else:
-                    if self._haveuselessterm(lastnode):  # step 4
-                        self._deleteuslessterm(lastnode)
-                    else:
-                        if self._isoperation(ptr):  # step 5
-                            self._deleteoperationterm(ptr)
-                            self._reformattree(ptr)
-                            break
-                        else:
-                            if not self._havenonterminalchilds(lastnode):  # step 6
-                                break
-            break
+                break
 
     def getlastnonterm(self, ptr) -> NodeStruct:
         '''Select the leftmost tree node marked with a nonterminal symbol'''
