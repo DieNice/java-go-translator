f5270ccc5cf2ca0dcd95f7dee46135e0fe74cc91
eswat109 <eswat98@mail.ru>
2021-04-19 18:25:28 +1000

    checkin id initialization added

--- translator/syntacticalanalyzer/semanticanalyzer/semanticalanalyzer.py
+++ translator/syntacticalanalyzer/semanticanalyzer/semanticalanalyzer.py
@@ -21,18 +21,30 @@ class Semanticalanalizer:
         self.inttypes = ['byte', 'short', 'int', 'long', 'char']
         self.floattypes = ['float', 'double']
         self.stringtypes = ['char', 'String']
+        self.exeptions: [str] = []
 
     def __settotype(self, node: NodeStruct, type: str):
         pass
 
-    def __getoperationtype(self, oper1: NodeStruct, oper2: NodeStruct):
-        type1 = self.__gettype(oper1)
-        type2 = self.__gettype(oper2)
-        if self.__istransformable(type1, type2):
-            return type2
-        elif self.__istransformable(type2, type1):
-            return type1
-        raise Exception('cant transform types {} and {}'.format(type1, type2))
+    def __getoperationtype(self, operation: str, operand1: NodeStruct, operand2: NodeStruct):
+        operationtype = None
+        operandtype1 = self.__gettype(operand1)
+        operandtype2 = self.__gettype(operand2)
+        if self.__istransformable(operandtype1, operandtype2):
+            operationtype = operandtype2
+        elif self.__istransformable(operandtype2, operandtype1):
+            operationtype = operandtype1
+        if not ((operationtype in self.inttypes + self.floattypes and \
+                operation in self.numbisoperations) or \
+            (operationtype in self.logtypes and \
+                operation in self.logbisoperations) or \
+            (operationtype in self.stringtypes and \
+                operation in self.strbisoperations)):
+            raise Exception('bad operand types for binary operator \'{}\': first type: {}, second type: {}'.format(operation ,operandtype1, operandtype2))
+
+        if not operationtype:
+            raise Exception('cant transform types {} and {}'.format(operandtype1, operandtype2))
+        return operationtype
 
     def __istransformable(self, typefrom: str, typeto: str) -> bool:
         if typefrom == typeto:
@@ -58,11 +70,10 @@ class Semanticalanalizer:
 
     def __gettype(self, ptr: NodeStruct):
         if ptr.name == 'IDENTIFICATOR':
-            tmp = self.env.searchelem(self.envptr, ptr.value)
-            if tmp != -1:
-                return tmp
+            if self.__getidflagfromscope(ptr.value):
+                return self.__getidtypefromscope(ptr.value)
             else:
-                raise Exception("identificator {} is not defined".format(ptr.value))
+                raise Exception('variable {} might not have been initialized'.format(ptr.value))
         elif ptr.name == 'INTEGER NUMBER':
             if -128 < int(ptr.value) < 127:
                 return 'byte'
@@ -85,7 +96,7 @@ class Semanticalanalizer:
             if len(ptr.childs) == 2:
                 '''for bis operations'''
                 if operation in self.bisoperations:
-                    return self.__getoperationtype(ptr.childs[0], ptr.childs[1])
+                    return self.__getoperationtype(operation, ptr.childs[0], ptr.childs[1])
                 else:
                     raise Exception('unknown bis operation {}'.format(operation))
             elif len(ptr.childs) == 1:
@@ -106,6 +117,28 @@ class Semanticalanalizer:
         self.__search(ptr)
         self.envptr = self.envptr.prev
 
+    def __addidtoscope(self, id: str, type: str, flag: bool = False):
+        self.envptr.addid(id, (type, flag))
+
+    def __updateidinscope(self, id: str):
+        tmp = self.env.searchelem(self.envptr, id)
+        if tmp == -1:
+            raise Exception("identificator {} is not defined".format(id))
+        type = tmp[0]
+        self.envptr.addid(id, (type, True))
+
+    def __getidtypefromscope(self, id: str):
+        tmp = self.env.searchelem(self.envptr, id)
+        if tmp == -1:
+            raise Exception("identificator {} is not defined".format(id))
+        return tmp[0]
+
+    def __getidflagfromscope(self, id: str):
+        tmp = self.env.searchelem(self.envptr, id)
+        if tmp == -1:
+            raise Exception("identificator {} is not defined".format(id))
+        return tmp[1]
+
     def __search(self, ptr: NodeStruct) -> None:
         for i in ptr.childs:
             if i.name in ['DECLARE A VARIABLES']:
@@ -113,21 +146,32 @@ class Semanticalanalizer:
                 type = i.value
                 for j in i.childs:
                     id = None
+                    flag = False
                     if j.name == 'ASSIGNMENT':
                         id = j.childs[0].value
                         inittype = self.__gettype(j.childs[1])
                         if not self.__istransformable(inittype, type):
                             raise Exception('incompatible types for id {} : {} cannot be converted to {}'.format(id, inittype, type))
+                        flag = True
                     elif j.name == 'IDENTIFICATOR':
                         id = j.value
+                        flag = False
                     else:
                         raise Exception('unknown declare variables')
-                    self.envptr.addid(id, type)
+                    self.__addidtoscope(id, type, flag)
                     j.idtable = self.envptr
-            if i.name in ['CYCLE FOR', 'IF OPERATOR']:
+            elif i.name in ['CYCLE FOR', 'IF OPERATOR']:
                 '''create new scope'''
                 self.__addscopefor(i)
                 continue
+            elif i.name in ['ASSIGNMENT']:
+                id = i.childs[0].value
+                type = self.__getidtypefromscope(id)
+                inittype = self.__gettype(i.childs[1])
+                if not self.__istransformable(inittype, type):
+                    raise Exception(
+                        'incompatible types for id {} : {} cannot be converted to {}'.format(id, inittype, type))
+                self.__updateidinscope(id)
             self.__search(i)
 
     def scan(self, ast: SyntacticsStructure):
